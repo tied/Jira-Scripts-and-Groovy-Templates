409f00fb2ab9d8e9e7dc36320cb2eb3913ed5d4f
mjurdyga <mjurdyga@me.com>
2019-11-05 14:37:28 +0000

    Update checkissue.groovy

--- checkissue.groovy
+++ checkissue.groovy
@@ -1,2 +1,4 @@
 issuefunction not in linkedissuesof('status != "DONE"', 'is cloned by')
 issue.subTaskObjects.every {it.getResolution()]
+                            
+issueFunction in subtasksOf("resolution = 'Fixed or Completed'")
