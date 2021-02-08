42a260fe586a2d7bb9c0fbf08bbbe0f055ca2211
mjurdyga <mjurdyga@me.com>
2021-02-08 12:45:27 +0000

    Create jira-python.py

---
+++ jira-python.py
@@ -0,0 +1,31 @@
+from jira import JIRA
+import re
+
+user = 'michal USER'
+apikey = 'MY API KEY'
+server = 'https://jira.collabd2.test-and-trace.nhs.uk'
+options = {
+ 'server': server
+}
+
+jira = JIRA(options, basic_auth=(user,apikey) )
+
+size = 100
+initial = 0
+projects = jira.projects()
+print (projects);
+
+
+##########Ver 2 with Simple auth
+from jira import JIRA
+import re
+
+#props = jira.application_properties()
+#projects = jira.projects()
+
+options = {'server': 'https://jira.devopsit.co.uk'}
+jira = JIRA(options, basic_auth=('admin', 'PASS'))
+size = 100
+initial = 0
+projects = jira.projects()
+print (projects);
