dd8e291c116a9c040d77aa9baf756813141c9e06
mjurdyga <mjurdyga@me.com>
2022-04-11 09:54:01 +0100

    Update jira-python.py

--- jira-python.py
+++ jira-python.py
@@ -3,7 +3,7 @@ import re
 
 user = 'michal USER'
 apikey = 'MY API KEY'
-server = 'https://jira.collabd2.test-and-trace.nhs.uk'
+server = 'https://myjirahost.jira'
 options = {
  'server': server
 }
@@ -23,7 +23,7 @@ import re
 #props = jira.application_properties()
 #projects = jira.projects()
 
-options = {'server': 'https://jira.devopsit.co.uk'}
+options = {'server': 'https://myjirahost.jira'}
 jira = JIRA(options, basic_auth=('admin', 'PASS'))
 size = 100
 initial = 0
